feef75883c4de1e0d50805fbc62eb92e6415a865
nunomagz <1251665@isep.ipp.pt>
2026-03-24 20:45:38 +0000

    finalização da classe EstudanteDAL.java e inicialização da classe EstudanteBLL.java

--- src/DAL/EstudanteDAL.java
+++ src/DAL/EstudanteDAL.java
@@ -2,36 +2,98 @@ package DAL;
 
 import Model.Estudante;
 import java.util.ArrayList;
-
+/**
+ * Camada (DAL) para a entidade Estudante.
+ * Responsável por armazenar e recuperar objetos Estudante em memória.
+ */
 public class EstudanteDAL {
-    private ArrayList<Estudante> listaEstudantes;
+    private ArrayList<Estudante> estudantes;
 
+    /**
+     * Construtor da classe EstudanteDAL
+     */
     public EstudanteDAL(){
-        listaEstudantes = new ArrayList<>();
+        estudantes = new ArrayList<>();
     }
 
-    public Estudante adicionarEstudante(Estudante estudante){
-        try {
-            listaEstudantes.add(estudante);
-            return estudante;
-        } catch (Exception e) {
-            System.out.println("Apenas para Gestores!");
+    /**
+     * Adiciona um novo estudante a lista.
+     * @param estudante O novo estudante.
+     */
+   public void adicionarEstudante(Estudante estudante){
+        estudantes.add(estudante);
+   }
+
+    /**
+     * Lista de estudantes registados no sistema.
+     * @return Lista de estudantes.
+     */
+   public ArrayList<Estudante> listarEstudantes(){
+        return new ArrayList<>(estudantes);
+   }
+
+    /**
+     * Atualiza os dados do estudante, identificado pelo número mecanográfico.
+     * @param estudanteAtualizado O Estudante com os dados atualizados.
+     * @return True se for atualizado com sucesso e False se não for encontrado.
+     */
+   public boolean atualizarEstudante(Estudante estudanteAtualizado){
+       for (int i = 0; i < estudantes.size();i++){
+           if(estudantes.get(i).getNumMecanografico().equals(estudanteAtualizado.getNumMecanografico())){
+               estudantes.set(i, estudanteAtualizado);
+               return true;
+           }
+       }
+       return false;
+   }
+
+    /**
+     * Remove o estudante da lista pelo número mecanográfico.
+     * @param estudante O estudante a remover.
+     */
+    public void removerEstudante(Estudante estudante){
+        estudantes.remove(estudante);
+    }
+
+    /**
+     * Procura um estudante por número mecanográfico.
+     * @param numMecanografico O número mecanográfico a procurar.
+     * @return O estudante encontrado ou null se não existir.
+     */
+    public Estudante procurarPorNumMecanografico(String numMecanografico) {
+        for (Estudante e : estudantes) {
+            if (e.getNumMecanografico().equals(numMecanografico)) {
+                return e;
+            }
         }
         return null;
     }
 
-    public Estudante procurarPorNumMecanografico(String numMecanografico){
-        boolean encontrado = false;
-        Estudante estudanteEncontrado = null;
-
-        for (int i = 0; i <listaEstudantes.size() && !encontrado;i++){
-            if (listaEstudantes.get(i).getNumMecanografico().equalsIgnoreCase(numMecanografico)){
-                encontrado = true;
-                estudanteEncontrado = listaEstudantes.get(i);
+    /**
+     * Procura o estudante por NIF
+     * @param nif O NIF a procurar.
+     * @return O estudante encontrado ou null se não existir.
+     */
+    public Estudante procurarPorNif(String nif){
+        for (Estudante e : estudantes) {
+            if (e.getNif().equals(nif)){
+                return e;
             }
-
         }
-        return estudanteEncontrado;
+        return null;
     }
 
+    /**
+     * Procura o estudante por E-mail.
+     * @param email O e-mail a procurar.
+     * @return O estudante encontrado ou null se não existir.
+     */
+    public Estudante procurarPorEmail(String email){
+        for (Estudante e :estudantes){
+            if (e.getEmail().equals(email)){
+                return e;
+            }
+        }
+        return null;
+    }
 }
