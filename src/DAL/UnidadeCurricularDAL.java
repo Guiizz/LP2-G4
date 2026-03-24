d12783ff1dba54923c1db88d4fb860c2a3c55e98
1251652 <1251652@isep.ipp.pt>
2026-03-24 22:21:30 +0000

    preenchimento CursoDAL e UnidadeCurricularDAL

--- src/DAL/UnidadeCurricularDAL.java
+++ src/DAL/UnidadeCurricularDAL.java
@@ -1,4 +1,34 @@
 package DAL;
 
+import Model.UnidadeCurricular;
+import java.util.ArrayList;
+
 public class UnidadeCurricularDAL {
+    private ArrayList<UnidadeCurricular> unidades;
+
+    public UnidadeCurricularDAL() {
+        unidades = new ArrayList<>();
+    }
+
+    public void adicionarUnidade(UnidadeCurricular unidade) {
+        unidades.add(unidade);
+    }
+
+    public boolean atualizarUnidade(UnidadeCurricular unidadeatualizado) {
+        for (int i = 0; i < unidades.size(); i++) {
+            if (unidades.get(i).equals(unidadeatualizado)){
+                unidades.set(i, unidadeatualizado);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<UnidadeCurricular> listarUnidades() {
+        return new ArrayList<>(unidades);
+    }
+
+    public void removerUnidade(UnidadeCurricular unidade) {
+        unidades.remove(unidade);
+    }
 }
